e7371aee201a9760f37d5834a9978041b1bff715
liumingjun <454335025@qq.com>
2016-12-27 17:32:03 +0800

    2016-12-27 微信绑定问题

--- app/controllers/wx_controllers/WxCommonController.php
+++ app/controllers/wx_controllers/WxCommonController.php
@@ -7,18 +7,18 @@ class WxCommonController
 
     public static function OAuth2($function = null)
     {
-        if ($function != null && function_exists($function)) {
+        if ($function != null && method_exists('WxCommonController',$function)) {
             $code = $_GET["code"];//获取code
             $url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=" . APPID . "&secret" . SECRET . "&code=$code&grant_type=authorization_code";//通过code换取网页授权access_token
             $str = file_get_contents($url);
             self::$oauth = json_decode($str, true);
-            return call_user_func($function);
+            return call_user_func(array('WxCommonController',$function));
         } else {
             echo "未找到方法";exit;
         }
     }
 
-    public static function getUserInfo()
+    function getUserInfo()
     {
         $url = "https://api.weixin.qq.com/sns/userinfo?access_token=" . self::$oauth['access_token'] . "&openid=" . self::$oauth['openid'];
         $str = file_get_contents($url);//获取用户信息
